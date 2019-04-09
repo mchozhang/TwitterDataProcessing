0e82452dd3c09933f510f2e52921afd2f9867422
Ivy Long <cacarianlong@gmail.com>
2019-04-10 09:27:44 +1000

    partition twitter file, code in test.py file -version2

--- test.py
+++ test.py
@@ -25,29 +25,42 @@ stop = (file_size // process_number) * (rank + 1)
 
 
 
-with open(filename) as twitter_file:
+with open(filename,"rb") as twitter_file:
     new_filename = "part_{}.json".format(rank)
-    new_file = open(new_filename, "w+")
+    new_file = open(new_filename, "wb")
     # trim each start line and stop line
     twitter_file.seek(start, 0)
     start_line = twitter_file.readline()
-
+ #   print(rank,":  the first line of a file-",repr(start_line),"-length:", len(start_line))
     twitter_file.seek(stop, 0)
     stop_line = twitter_file.readline()
 
-    start += len(start_line)+1 # start cursor at head of next line
+    start += len(start_line) # start cursor at head of next line
+#    print(rank,": the start position: ",start,twitter_file.readline())
     stop += len(stop_line)-1 # stop cursor at tail of this line
     twitter_file.seek(start, 0)
-    print("\n","this is the start line \n", twitter_file.readline())
+#    print(rank, ": the stop position: ", stop)
+#    print("\n process",rank,"\n this is the start line \n", twitter_file.readline()[0:50])
     twitter_file.seek(stop, 0)
-   # print("\n this is the end line \n", twitter_file.readline())
+
+#    print("\n process",rank,"\n this is the end line \n", twitter_file.readline()[0:50])
     cur_pos = start
     twitter_file.seek(cur_pos, 0)
     # read file and partition it
+    print("curstart",cur_pos)
     for line in twitter_file:
-        cur_pos += len(line)-1
+        # if rank == 0 and cur_pos <30000:
+        #     print("process: ",rank,":cur_pos:",cur_pos,"stop:",stop,line[0:30])
+        cur_pos += len(line)
         if cur_pos > stop:
-      #      print("\n this is super line \n",line)
+                #            print("process: ", rank,"\n this is super line \n",line[0:50])
             break
+
         new_file.write(line)
+    # while(cur_pos<stop):
+    #     line = twitter_file.readline()
+    #     cur_pos += len(line)-1
+    #     # print("process: ", rank,"\n this is super line \n",line[0:50]
+    #     new_file.write(line)
+    #     twitter_file.seek(cur_pos,0)
     new_file.close()
