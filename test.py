8937ac568b8808b6cb15092dd66ad5fd26bdf88b
Ivy Long <cacarianlong@gmail.com>
2019-04-09 17:46:35 +1000

    update test.py

--- test.py
+++ test.py
@@ -29,20 +29,25 @@ with open(filename) as twitter_file:
     new_filename = "part_{}.json".format(rank)
     new_file = open(new_filename, "w+")
     # trim each start line and stop line
-    twitter_file.seek(start,0)
+    twitter_file.seek(start, 0)
     start_line = twitter_file.readline()
-    twitter_file.seek(stop,0)
-    stop_line = twitter_file.readline()
-    start += len(start_line)
-    stop += len(stop_line)
 
+    twitter_file.seek(stop, 0)
+    stop_line = twitter_file.readline()
+
+    start += len(start_line)+1 # start cursor at head of next line
+    stop += len(stop_line)-1 # stop cursor at tail of this line
+    twitter_file.seek(start, 0)
+    print("\n","this is the start line \n", twitter_file.readline())
+    twitter_file.seek(stop, 0)
+   # print("\n this is the end line \n", twitter_file.readline())
     cur_pos = start
     twitter_file.seek(cur_pos, 0)
     # read file and partition it
     for line in twitter_file:
-        cur_pos += len(line)
+        cur_pos += len(line)-1
         if cur_pos > stop:
+      #      print("\n this is super line \n",line)
             break
         new_file.write(line)
-
     new_file.close()
