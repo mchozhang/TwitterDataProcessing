04464eafb6a933925ff290ad03afcb3e12d5a5b8
McHo Zhang <vivazac@qq.com>
2019-04-10 20:08:18 +1000

    output format

--- geo_util.py
+++ geo_util.py
@@ -131,3 +131,78 @@ class GridManager(object):
             return None
 
         return chr(ord("A") + index_y - 1) + str(index_x)
+
+
+def sum_up_post_counter(posts_counter_list):
+    """
+    sum up the final posts result from the counter list
+    :param posts_counter_list: list of posts counter result gathered from the communication domain
+    :return: final posts counter
+    """
+    final_posts_counter = Counter()
+    for counter in posts_counter_list:
+        final_posts_counter.update(counter)
+    return final_posts_counter
+
+
+def sort_post_counter(counter):
+    """
+    sort the post counter in descendant order
+    :param counter: post counter
+    :return: list of tuples, in form of [(A1, 100), (A2, 90)]
+    """
+    posts_counter_list = sorted(counter.items(), key=lambda kv: kv[1])
+    posts_counter_list.reverse()
+    return posts_counter_list
+
+
+def print_post_result(counter_list):
+    """
+    print the result of the counter in order
+    :param counter_list: post counter list
+    :return:
+    """
+    for cell_name, number in counter_list:
+        print("{} : {} posts".format(cell_name, number))
+
+
+def sum_up_hashtags_counter(post_counter, hashtags_counter_table_list):
+    """
+    sum up the hashtags counter
+    :param post_counter: counter of posts of all the cell
+    :param hashtags_counter_table_list: list of gathered hashtags counter table
+    :return:
+    """
+    hashtags_counter_table = {name: Counter() for name in post_counter.keys()}
+    for table in hashtags_counter_table_list:
+        for cell_name, counter in table.items():
+            hashtags_counter_table[cell_name].update(counter)
+    return hashtags_counter_table
+
+
+def print_hashtags_counter(hashtags_counter_table, posts_counter_list):
+    """
+    print the top 5 hashtags counter of every cell
+    :param hashtags_counter_table: hashtag counter of all the cells
+    :param posts_counter_list: sorted list of the posts counter of all the cells
+    :return:
+    """
+    for cell_name, posts_number in posts_counter_list:
+        # e.g. {"100", ["melb", "vic"], "99": ["baby"]}
+        cell_result_table = dict()
+        cell_value_rank = []
+
+        for hashtag, number in hashtags_counter_table[cell_name].most_common():
+            hashtag = "#" + hashtag
+            if cell_result_table.get(number) is not None:
+                cell_result_table[number].append(hashtag)
+            elif len(cell_result_table.keys()) >= 5:
+                break
+            else:
+                cell_value_rank.append(number)
+                cell_result_table[number] = [hashtag]
+
+        print_item = cell_name + " : "
+        for number in cell_value_rank:
+            print_item += "(" + ", ".join(cell_result_table[number]) + ", " + str(number) + "), "
+        print(print_item[:-2])
